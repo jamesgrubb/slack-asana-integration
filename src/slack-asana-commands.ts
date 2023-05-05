10bc9d209a3b3901ea433906a3f4c159ca468689
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2023-05-05 20:19:51 +0100

    added slack/bolt

--- src/slack-asana-commands.ts
+++ src/slack-asana-commands.ts
@@ -1,8 +1,15 @@
-import type { Handler } from "@netlify/functions";
+const { App } = require("@slack/bolt");
 
-export const handler: Handler = async () => {
-  return {
-    statusCode: 200,
-    body: "ok",
-  };
-};
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+// All the room in the world for your code
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
